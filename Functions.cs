551e2de109bdcff1b8af6d57699a26638c788537
Amina <78281034+aminasab@users.noreply.github.com>
2023-01-04 22:41:18 +0300

    Add files via upload

---
+++ Functions.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System. Collections. Generic;
+using System. Linq;
+using System. Text;
+using System. Threading. Tasks;
+using static System. Formats. Asn1. AsnWriter;
+
+namespace TheFirstProgram
+    {
+    public class Functions
+        {
+        /// <summary>
+        /// Метод, который строит горизонтальные строки
+        /// </summary>
+
+        public static void constructionOfTheHorizontLines( int horizontalLine , ref int score , int verticalLine , string line )
+            {
+            if (score==0||score==( line. Length+2 )||score==( verticalLine )||score==( ( line. Length+2 )*2 ))
+                {
+                string horizontalLines =  new String('+' , horizontalLine); //построение горизонтальных строк
+                Console. WriteLine (horizontalLines);
+                }
+            }
+
+
+        /// <summary>
+        /// Метод, который строит 1 строку
+        /// </summary>
+
+        public static void constructionOfTheFirstLine( int result , int horizontalLine , string line , ref int score )
+            {
+            if (score==0)
+                {
+                score++;
+                }
+
+            if (( line. Length/2 )>score||score<( line. Length+2 )) //построение 1 строки
+                {
+                string emptyString = new String(' ' , horizontalLine-2);
+                string thefirst = '+'+emptyString+'+';
+                Console. WriteLine (thefirst);
+
+                if (score==( line. Length/2 ))
+                    {
+                    score++;
+                    int distanceForPadLeft = ( result-1 )+line. Length; // формула нахождения расстояния для PadLeft
+                    string concatStringAndSpace = '+'+line. PadLeft (distanceForPadLeft)+"+". PadLeft (horizontalLine-distanceForPadLeft-1); //конкатенация символа и слова с пробелами для 1 строки
+                    Console. WriteLine (concatStringAndSpace);
+                    }
+                }
+            }
+
+        /// <summary>
+        /// Метод, который строит 2 строку
+        /// </summary>
+
+        public static void constructionOfTheSecondLine( int horizontalLine , string line , ref int score )
+            {
+            if (score>( line. Length+2 )&&score<( ( line. Length+2 )*2 )) //построение 2 строк
+                {
+                string secondLine = string. Concat (Enumerable. Repeat ("+ " , ( ( horizontalLine-2 )/2 ))); //построение 2 строки
+                string thesecond = secondLine+'+'+'+';
+                Console. WriteLine (thesecond);
+
+                if (score!=( line. Length+2 )*2-1)
+                    {
+                    score++;
+                    }
+
+                if (score<( line. Length+2 )*2&&score!=( line. Length+2 )*2-1)
+                    {
+                    string thethird = '+'+secondLine+'+';
+                    Console. WriteLine (thethird);
+                    }
+                }
+            }
+
+        /// <summary>
+        /// Метод, который строит 3 строку
+        /// </summary>
+
+        public static void constructionOfTheThirdLine( int horizontalLine , ref int score , int verticalLine , string line )
+            {
+            if (score==(((line.Length+2)*2)+1)&&score<verticalLine)
+                {
+                int lenghtTheThirdLine = horizontalLine-2;
+                for (int i = 1 ; i<=lenghtTheThirdLine ; i++)
+                    {
+                    for (int j = 1 ; j<=horizontalLine ; j++)
+                        {
+                        if (( i+1 )==j||j==1||j==( horizontalLine-i ))
+                            {
+                            Console. Write ('+');
+                            }
+                        else if (j==horizontalLine)
+                            {
+                            Console. WriteLine ('+');
+                            }
+                        else
+                            {
+                            Console. Write (' ');
+                            }
+                        }
+                    if (score!=verticalLine-1)
+                        {
+                        score++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿namespace TheFirstProgram
+    {
+    internal class Program
+        {
+        static void Main( string[] args )
+            {
+            string? line;
+            int result = 0;
+
+            do //проверка введенных данных
+                {
+                Console. WriteLine ("Введите размерность таблицы (число в диапазоне от 1 до 6)");
+                string? check = Console. ReadLine ();
+                try
+                    {
+                    result=int. Parse (check);
+                    }
+                catch (FormatException)
+                    {
+                    Console. WriteLine ("Введена "+check);
+                    }
+
+                if (1<=result&result<=6)
+                    {
+                    Console. WriteLine ("Данные введены верно");
+                    }
+                else Console. WriteLine ("Попробуйте ввести еще раз, число должно быть в диапазоне от 1 до 6");
+                } while (result>6|result<1);
+
+            do
+                {
+                int limit = 40-( result*2-2 );
+                Console. WriteLine ($"Введите строку, состоящую из не более {limit} символов");
+                line=Console. ReadLine (). Trim ();
+                } while (line. Length==0);
+
+            Console. WriteLine ("Данные введены успешно");
+            Console. Clear ();
+            int horizontalLine = result*2+line. Length; // нахождение длины горизонтальной линии
+            int verticalLine = ( line. Length+1 )*4+3; // нахождение длины вертикальной линии
+
+            for (int score = 0 ; score<=( verticalLine ) ; score++)
+                {
+                for (int lineCout = 0 ; lineCout<=3 ; lineCout++)
+                    {
+                    switch (lineCout)
+                        {
+                        case 0:
+                            Functions. constructionOfTheHorizontLines (horizontalLine , ref score , verticalLine , line);
+                            break;
+                        case 1:
+                            Functions. constructionOfTheFirstLine (result , horizontalLine , line , ref score);
+                            break;
+                        case 2:
+                            Functions. constructionOfTheSecondLine (horizontalLine , line , ref score);
+                            break;
+                        case 3:
+                            Functions. constructionOfTheThirdLine (horizontalLine , ref score , verticalLine , line);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
